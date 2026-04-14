0727e8e8cf7bc95bbe8429debbc75637a34f6fe6
not-iron-yury <not.iron.yuriy@inbox.ru>
2026-04-14 15:11:04 +0300

    feat(vitepress): vue компоненты проекта

---
+++ .vitepress/theme/components/Accordions.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue';
+
+const open = ref(null);
+
+const toggle = (index) => {
+  open.value = open.value === index ? null : index;
+};
+</script>
+
+<template>
+  <div class="accordion">
+    <div class="item">
+      <button @click="toggle(1)">Что входит в услугу?</button>
+
+      <div v-show="open === 1">В услугу входит аудит сайта и внедрение SEO-рекомендаций.</div>
+    </div>
+
+    <div class="item">
+      <button @click="toggle(2)">Сколько времени занимает оптимизация?</button>
+
+      <div v-show="open === 2">Базовая оптимизация занимает 2–4 недели.</div>
+    </div>
+  </div>
+</template>
---
+++ .vitepress/theme/components/Marker.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue';
+
+const activeTab = ref('tab1');
+</script>
+
+<template>
+  <div class="tabs">
+    <div class="tabs-nav">
+      <button @click="activeTab = 'tab1'">Вкладка 1</button>
+      <button @click="activeTab = 'tab2'">Вкладка 2</button>
+      <button @click="activeTab = 'tab3'">Вкладка 3</button>
+    </div>
+
+    <div class="tabs-content">
+      <div v-show="activeTab === 'tab1'">контент 1</div>
+
+      <div v-show="activeTab === 'tab2'">контент 2</div>
+
+      <div v-show="activeTab === 'tab3'">контент 3</div>
+    </div>
+  </div>
+</template>
